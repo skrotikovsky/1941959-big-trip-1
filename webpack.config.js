3625c43983533a3465f96cfe259bd594c9869814
skrut <skrutikovsky@yandex.ru>
2022-02-28 16:05:29 +0500

    Поправил косяки предыдущей версии

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname,'main.js'),
+  entry: path.resolve(__dirname,'./src/main.js'),
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
@@ -12,11 +12,11 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: ['babel-loader']
-      }
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: ['babel-loader']
+        }
     ]
   }
 }
